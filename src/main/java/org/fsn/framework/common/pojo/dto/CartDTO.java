302274e8dc7b41d00f8944a5038a287e7e29525b
Sorta <sorta@163.com>
2020-01-10 17:14:00 +0800

    UnsafeInstance

--- src/main/java/org/fsn/framework/common/pojo/dto/CartDTO.java
+++ src/main/java/org/fsn/framework/common/pojo/dto/CartDTO.java
@@ -1,7 +1,13 @@
 package org.fsn.framework.common.pojo.dto;
 
-public class CartDTO {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CartDTO {
+    @JsonIgnore
+    public  static String  DEFALUTP ="shop";
+    private String p;//项目名称
     private Long uid; //用户id
     private Long pid; //产品id
     private Long gid; //货品id
@@ -9,6 +15,15 @@ public class CartDTO {
     private Integer an;  //活动编号
     private Long ct;// 创建时间
     private Integer count;//数量
+    private Integer cc;//  增加减少数量使用,添加传正数，减少传负数
+
+    public String getP() {
+        return p;
+    }
+
+    public void setP(String p) {
+        this.p = p;
+    }
 
     public Long getUid() {
         return uid;
@@ -64,5 +79,13 @@ public class CartDTO {
 
     public void setCount(Integer count) {
         this.count = count;
+    }
+
+    public Integer getCc() {
+        return cc;
+    }
+
+    public void setCc(Integer cc) {
+        this.cc = cc;
     }
 }
