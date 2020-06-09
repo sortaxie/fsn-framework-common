4bd5a480c34549c6b267c2c8673ec44b4c85543e
Sorta <sorta@163.com>
2020-06-09 10:45:58 +0800

    UnsafeInstance

---
+++ src/main/java/org/fsn/framework/common/utils/CacheConstant.java
@@ -0,0 +1,7 @@
+package org.fsn.framework.common.utils;
+
+public class CacheConstant {
+
+    public static  String SEPARATOR = ":";
+    public static  String PRODUCT_DETAIL = "cache_product_detail";
+}
